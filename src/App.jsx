3cba20ae01c766d537d1223eb34faeed69b8b9b7
Hataf Atif <hatafbinatif@gmail.com>
2023-01-20 01:37:37 +0500

    Simple Block to play with

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css";
+
+import { motion } from "framer-motion";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    const [refresh, setRefresh] = useState(false);
+    return (
+        <motion.div
+            className="box"
+            initial={{opacity: 0}}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1.1 }}
+            drag={true}
+            dragConstraints={{ left: -100, right: 100}}
+            whileInView={{opacity: 1}}
+        >
+            <div className="simple-text">Damn!</div>
+        </motion.div>
+    );
+};
 
-export default App
+export default App;
