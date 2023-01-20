b766e456868327eb1ba0cd8d088db43282ff1b7f
Hataf Atif <hatafbinatif@gmail.com>
2023-01-20 17:42:37 +0500

    navbar pop in from top

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ import "./App.css";
 
 import { motion } from "framer-motion";
 
+const navbarVariant = {
+    hiddenTop: { y: "-10vh" },
+    slideDown: { y: 0, 
+        transition: {
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 0.5,
+            
+        }
+},
+};
+
 const App = () => {
-    const [refresh, setRefresh] = useState(false);
     return (
         <motion.div
-            className="box"
-            initial={{opacity: 0.1}}
-            whileHover={{opacity: 1}}
+            className="navbar"
+            animate={"slideDown"}
+            initial={"hiddenTop"}
+            variants={navbarVariant}
         >
-            <div className="simple-text">Damn!</div>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
         </motion.div>
     );
 };
